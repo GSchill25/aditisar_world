e4c782d8530be5d5fd9ea03a741b8f8b1fa596e5
Graham Schilling <gschilli@andrew.cmu.edu>
2014-04-29 19:59:32 -0400

    set up article model

---
+++ app/models/article.rb
@@ -0,0 +1,9 @@
+class Article < ActiveRecord::Base
+	belongs_to :category
+
+	validates_presense_of :title
+	validates_presense_of :content
+
+	scope :active, where('active = ?', true)
+  	scope :alphabetical, order('title')
+end
